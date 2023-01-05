6ef89c8b5e76da6dea1457795a1cbf0860bcc725
TriKang <ysck73@gmail.com>
2023-01-05 11:31:17 +0900

    http 요청 메시지 해독을 위한 테스트 코드

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-str = "PUT / gameserver/status / HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-agent: GameClient/1.0\r\n"
-# print(str.split(' / '))
-print(str.split('\r\n')[0])
+str = "PUT gameserver/status HTTP/1.1\r\nHost: 127.0.0.1\r\nUser-agent: GameClient/1.0\r\n"
+str2 = 'POST gameserver/itembuy/b HTTP/1.1\r\nHost: 127.0.0.1\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nUser-agent: GameClient/1.0\r\n\r\n{"item": 1}'
+
+ # print(str.split(' / '))
+print(str2)
+print(str2.split('\r\n')[0].split(' '))
+
+print('===')
+for msg in str2.split('\r\n')[1:]:
+    if not msg:
+        # body 영역임을 감지
+        continue
+    
+    print(msg)
+
+# print(str2.split('\r\n')[1:])
